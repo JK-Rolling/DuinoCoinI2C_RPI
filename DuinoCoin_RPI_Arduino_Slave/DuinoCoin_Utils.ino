ac9508d5a39b1ca5a9df5a73d759b79f81de3133
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2021-10-10 11:57:46 +0800

    add CRC8 checksum

--- DuinoCoin_RPI_Arduino_Slave/DuinoCoin_Utils.ino
+++ DuinoCoin_RPI_Arduino_Slave/DuinoCoin_Utils.ino
@@ -104,3 +104,27 @@ bool check(String message)
     }
     return false;
 }
+
+// https://stackoverflow.com/questions/51731313/cross-platform-crc8-function-c-and-python-parity-check
+uint8_t crc8( uint8_t *addr, uint8_t len) {
+      uint8_t crc=0;
+      for (uint8_t i=0; i<len;i++) {
+         uint8_t inbyte = addr[i];
+         for (uint8_t j=0;j<8;j++) {
+             uint8_t mix = (crc ^ inbyte) & 0x01;
+             crc >>= 1;
+             if (mix) 
+                crc ^= 0x8C;
+         inbyte >>= 1;
+      }
+    }
+   return crc;
+}
+
+uint8_t str_crc8( String msg )
+{
+    int msg_len = msg.length() + 1;
+    char char_array[msg_len];
+    msg.toCharArray(char_array, msg_len);
+    return crc8(char_array,msg.length());
+}
