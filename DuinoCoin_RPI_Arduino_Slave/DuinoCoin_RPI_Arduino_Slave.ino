ae31c16c836334aeb384621b10c865733f27ca60
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2021-10-24 12:57:57 +0800

    self-reset if stuck

--- DuinoCoin_RPI_Arduino_Slave/DuinoCoin_RPI_Arduino_Slave.ino
+++ DuinoCoin_RPI_Arduino_Slave/DuinoCoin_RPI_Arduino_Slave.ino
@@ -6,11 +6,30 @@
   Modified by JK Rolling
 */
 
+// **WARNING**
+// Only uncomment WDT_EN if the AVR is using Optiboot bootloader
+// Old bootloader will stuck in boot loop due to existing bug
+// Only power cycle can make it responsive for 8s - reprogram within this 8s with WDT_EN commented
+
+// auto reset if no share is returned. assume avr hung
+//#define WDT_EN "Optiboot"
+
+#ifdef WDT_EN
+  #include <avr/wdt.h>
+#endif
+
 void setup() {
+  #ifdef WDT_EN
+    wdt_disable();
+  #endif
   Serial.begin(115200);
   DuinoCoin_setup();
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+  #ifdef WDT_EN
+    wdt_enable(WDTO_8S);
+  #endif
+  Serial.println("Startup Done!");
 }
 
 void loop() {
@@ -19,6 +38,9 @@ void loop() {
     Serial.print(F("Job Done : "));
     Serial.println(DuinoCoin_response());
     Blink();
+    #ifdef WDT_EN
+      wdt_reset();
+    #endif
   }
 }
 
