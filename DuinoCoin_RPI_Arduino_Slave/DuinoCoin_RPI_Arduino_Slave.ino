5e8baa1b9c4c7803fcadfae44790fc06f9d8a031
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2021-11-17 21:23:03 +0800

    optional serial. light up when working

--- DuinoCoin_RPI_Arduino_Slave/DuinoCoin_RPI_Arduino_Slave.ino
+++ DuinoCoin_RPI_Arduino_Slave/DuinoCoin_RPI_Arduino_Slave.ino
@@ -6,47 +6,71 @@
   Modified by JK Rolling
 */
 
+
 // **WARNING**
 // Only uncomment WDT_EN if the AVR is using Optiboot bootloader
-// Old bootloader will stuck in boot loop due to existing bug
+// Old bootloader will stuck in boot loop
 // Only power cycle can make it responsive for 8s - reprogram within this 8s with WDT_EN commented
 
-// auto reset if no share is returned. assume avr hung
-//#define WDT_EN "Optiboot"
+#define WDT_EN "Optiboot"
+//#define SERIAL_LOGGER Serial
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifdef WDT_EN
   #include <avr/wdt.h>
+#endif
+
+#ifdef SERIAL_LOGGER
+  #define SerialBegin()              SERIAL_LOGGER.begin(115200);
+  #define SerialPrint(x)             SERIAL_LOGGER.print(x);
+  #define SerialPrintln(x)           SERIAL_LOGGER.println(x);
+#else
+  #define SerialBegin()
+  #define SerialPrint(x)
+  #define SerialPrintln(x)
 #endif
 
 void setup() {
   #ifdef WDT_EN
     wdt_disable();
   #endif
-  Serial.begin(115200);
-  DuinoCoin_setup();
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+  SerialBegin();
+  DuinoCoin_setup();
   #ifdef WDT_EN
     wdt_enable(WDTO_8S);
   #endif
-  Serial.println("Startup Done!");
+  SerialPrintln("Startup Done!");
 }
 
 void loop() {
   if (DuinoCoin_loop())
   {
-    Serial.print(F("Job Done : "));
-    Serial.println(DuinoCoin_response());
-    Blink();
+    SerialPrint(F("Job Done : "));
+    SerialPrintln(DuinoCoin_response());
+    ledOff();
     #ifdef WDT_EN
       wdt_reset();
     #endif
   }
 }
 
-void Blink()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
+void ledOn() {
+    // Turn on the built-in led
+    #if defined(ARDUINO_ARCH_AVR)
+        PORTB = PORTB & B11011111;
+    #else
+        digitalWrite(LED_BUILTIN, HIGH);
+    #endif
+}
+
+void ledOff() {
+    // Turn off the built-in led
+    #if defined(ARDUINO_ARCH_AVR)
+        PORTB = PORTB | B00100000;
+    #else
+        digitalWrite(LED_BUILTIN, LOW);
+    #endif
 }
