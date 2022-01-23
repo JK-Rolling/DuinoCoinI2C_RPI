a9f77618743eabd06a17ea9c5e767cdd9c8a051d
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2022-01-23 09:29:01 +0800

    Update sha1.h

--- DuinoCoin_Tiny_Slave_trinket/sha1.h
+++ DuinoCoin_Tiny_Slave_trinket/sha1.h
@@ -8,6 +8,7 @@
   Cryptosuite-master sub-directory with the needed sha1.h/cpp files copied
   from there and modified as described above.
 */
+#pragma GCC optimize ("-O2")
 #ifndef Sha1_h
 #define Sha1_h
 
