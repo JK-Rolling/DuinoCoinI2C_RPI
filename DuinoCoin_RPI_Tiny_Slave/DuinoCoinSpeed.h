004aeecba7c9dffcf348a35e50a161aa9da3df41
JK-Rolling <89238608+JK-Rolling@users.noreply.github.com>
2022-01-23 09:22:21 +0800

    Add files via upload

---
+++ DuinoCoin_RPI_Tiny_Slave/DuinoCoinSpeed.h
@@ -0,0 +1,60 @@
+/*
+ * Description:
+ *      This file will provide additional information to the compiler
+ *      By defining SPEED value, the optimization can be changed during macro expansion phase
+ *      Default SPEED value is 2 (-O2)
+ *      
+ *      Example Usage:
+ *          #include "DuinoCoinSpeed.h"
+ *          // this should gives 
+ *          //      #pragma GCC optimize ("-O2")
+ * 
+ * Author: JK Rolling
+ * Date: 2022/01/20
+ * 
+ */
+
+#ifndef DUINOCOINSPEED_H_
+#define DUINOCOINSPEED_H_
+
+#define OPTIMIZATION(x) _Pragma (#x)
+
+// user to change SPEED preference
+#ifndef SPEED
+  #define SPEED 2
+#endif
+
+// 0 -O0    <- reduce compilation time
+// 1 -Os    <- optimize for size
+// 2 -O2    <- Fast
+// 3 -O3    <- Faster
+// 4 -Ofast <- Fastest
+// g -Og    <- optimize debugging experience
+#if SPEED==0
+  OPTIMIZATION (GCC optimize ("-O0"))
+#elif SPEED==1
+  OPTIMIZATION (GCC optimize ("-Os"))
+#elif SPEED==2
+  OPTIMIZATION (GCC optimize ("-O2"))
+#elif SPEED==3
+  OPTIMIZATION (GCC optimize ("-O3"))
+#elif SPEED==4
+  OPTIMIZATION (GCC optimize ("-Ofast"))
+#elif SPEED==g
+  OPTIMIZATION (GCC optimize ("-Og"))
+#else
+  #error "Invalid SPEED value. Use 0/1/2/3/4/g"
+#endif
+
+#endif
+
+/*
+ * Tested on Arduino Nano 328p
+ * 0 --> 145H/s
+ * 1 --> 241H/s
+ * 2 --> 268H/s (fastest)
+ * 3 --> 265H/s
+ * 4 --> 265H/s
+ * g --> 145H/s
+ * 
+ */
